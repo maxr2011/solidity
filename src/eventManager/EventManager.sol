0d566d5abdeffc84050233a8abd8d5910e889983
maxr2011 <maxi.rehberger@gmail.com>
2020-05-20 02:34:31 +0200

    eventManager: adding governance contract (EventManager)

---
+++ src/eventManager/EventManager.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.6.7;
+
+import "eventManager/Event.sol" as EventContract;
+
+// Governance Contract 
+// This contract manages 
+// - User Account Creation 
+// - Event Creation and Management by the User itself 
+contract EventManager {
+    
+    // variables 
+    // todo do we save only the addresses or the whole event contracts in the event manager 
+    
+    // user authentication 
+    
+    // event creation 
+    
+    
+}
