5b0f3cbd203016c397155037026e260b30087239
maxr2011 <maxi.rehberger@gmail.com>
2020-05-27 14:33:37 +0200

    eventManager: user mapping implemented in EventSet

--- src/eventManager/Storage/EventSet.sol
+++ src/eventManager/Storage/EventSet.sol
@@ -9,7 +9,10 @@ contract EventSet {
     
     // Mapping from address to position in the array
     // 0 means the address is not in the array
-    mapping (address => uint) index;
+    mapping (EventContract.Event => uint) index;
+    
+    // Mapping from user to its event array
+    mapping (address => EventContract.Event[]) user_mapping;
 
     // Array with address 
     EventContract.Event[] event_store;
@@ -42,20 +45,27 @@ contract EventSet {
     // add new element to array
     function addToArray(EventContract.Event event_element) public 
     {
+        
         // check for invalid event 
         require(address(event_element) != address(0), "Invalid Event");
         
         // check if already in array 
-        require(!inArray(address(event_element)), "Event already in Array");
+        require(!inArray(event_element), "Event already in Array");
         
         // append
-        index[address(event_element)] = event_store.length;
+        index[event_element] = event_store.length;
         event_store.push(event_element);
+        
+        // add initiator to user mapping 
+        address event_initiator = event_element.getInitiator();
+        
+        // add event to user mapping 
+        user_mapping[event_initiator].push(event_element);
         
     }
 
     // check if element is in array
-    function inArray(address event_address) public view returns (bool in_array) 
+    function inArray(EventContract.Event event_address) public view returns (bool in_array) 
     {
         
         // event 0x0 is not valid
@@ -64,11 +74,11 @@ contract EventSet {
     }
 
     // get position by specific element 
-    function getPosition(address event_address) public view returns (uint address_position)
+    function getPosition(EventContract.Event event_address) public view returns (uint address_position)
     {
         
         // Address 0x0 is not valid 
-        if(event_address == address(0)) return 0;
+        if(address(event_address) == address(0)) return 0;
         return index[event_address];
         
     }
@@ -120,61 +130,72 @@ contract EventSet {
     function getUserEventArray(address payable event_initiator, bool check_participant) public view returns(EventContract.Event[] memory event_array)
     {
         
-        // create new variable to count user events 
-        uint user_event_count = 0;
-        
-        // loop array to count elements 
-        for(uint i = 0; i < event_store.length; i++) {
+        if(!check_participant) {
             
-            // skip 0x0 event element 
-            if(i > 0) {
+            // get events from user mapping 
+            return user_mapping[event_initiator];
+            
+        } else {
+        
+        
+            // create new variable to count user events 
+            uint user_event_count = 0;
+        
+            // loop array to count elements 
+            for(uint i = 0; i < event_store.length; i++) {
+            
+                // skip 0x0 event element 
+                if(i > 0) {
                 
-                EventContract.Event temporary_event = event_store[i];
+                    EventContract.Event temporary_event = event_store[i];
                 
-                if(temporary_event.isInitiator(event_initiator) || (check_participant && temporary_event.isParticipant(event_initiator))) {
+                    if(temporary_event.isParticipant(event_initiator)) {
                     
-                    user_event_count++;
+                        user_event_count++;
                     
+                    }
+                
                 }
-                
+            
             }
+        
+        
+            // initialize new array with size of the user events 
+            event_array = new EventContract.Event [](user_event_count);
+        
+            // initialize new counter 
+            uint k = 0;
+        
+            // get all elements without the 0x0 event element 
+            for(uint j = 0; j < event_store.length-1; j++) {
             
-        }
-        
-        
-        // initialize new array with size of the user events 
-        event_array = new EventContract.Event [](user_event_count);
-        
-        // initialize new counter 
-        uint k = 0;
-        
-        // get all elements without the 0x0 event element 
-        for(uint j = 0; j < event_store.length-1; j++) {
+                // set a temporary event element variable 
+                EventContract.Event temporaryEvent = event_store[j+1];
             
-            // set a temporary event element variable 
-            EventContract.Event temporaryEvent = event_store[j+1];
-            
-            // check if user is the initiator of the event
-            if(temporaryEvent.isInitiator(event_initiator) || (check_participant && temporaryEvent.isParticipant(event_initiator))) {
+                // check if user is the initiator of the event
+                if(temporaryEvent.isParticipant(event_initiator)) {
                 
-                // add event element to output array 
-                event_array[k] = temporaryEvent;
-                k++;
+                    // add event element to output array 
+                    event_array[k] = temporaryEvent;
+                    k++;
                 
+                }
+            
             }
-            
-        }
         
-        // return array 
-        return event_array;
+            // return array 
+            return event_array;
+        
+        }
         
     }
     
     // remove specific address element 
-    function removeFromArray(address event_address) public
+    function removeFromArray(EventContract.Event event_address) public
     {
+        
         // check if in array 
-        require(inArray(event_address), "Address not in Array");
+        require(inArray(event_address), "Event not in Array");
         
         // move all other elements 
         uint pos = getPosition(event_address);
@@ -182,14 +203,40 @@ contract EventSet {
         for (uint i = pos; i < event_store.length-1; i++){
             
             // move next element to current element
-            index[address(event_store[i+1])] = index[address(event_store[i])];
+            index[event_store[i+1]] = index[event_store[i]];
             event_store[i] = event_store[i+1];
             
         }
         
+        // get initator 
+        address event_initiator = event_address.getInitiator();
+        
         // Remove the last element of the array
-        delete index[address(event_address)];
+        delete index[event_address];
         event_store.pop();
+        
+        // Remove from user mapping 
+        
+        // new variable for event position in mapping 
+        uint event_pos = 0;
+        
+        // move positions 
+        for(uint j = 0; j < user_mapping[event_initiator].length; j++) {
+            
+            if(user_mapping[event_initiator][j] == event_address) {
+                event_pos = j;
+            }
+            
+        }
+        
+        for(uint l = event_pos; l < user_mapping[event_initiator].length-1; l++) {
+            
+            user_mapping[event_initiator][l] = user_mapping[event_initiator][l+1];
+            
+        }
+        
+        // pop last element 
+        user_mapping[event_initiator].pop();
         
     }
     
