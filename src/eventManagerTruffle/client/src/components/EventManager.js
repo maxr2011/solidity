bf9752d6f9468d120a4c9814915e657530d9e51f
Maximilian Rehberger <maxi.rehberger@gmail.com>
2020-07-22 23:56:58 +0200

    eventManagerTruffle: EventManager.js updated

--- src/eventManagerTruffle/client/src/components/EventManager.js
+++ src/eventManagerTruffle/client/src/components/EventManager.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
+function doSomething() {
+    console.log("do something log");
+}
+
 class EventManager extends React.Component {
+
+    doSomething() {
+        console.log("do something");
+    }
+
     render() {
         return (
-            <h2>EventManager Test</h2>
+            <div id="buttonWrapper">
+                <h2>EventManager Test</h2>
+                <button onClick={this.doSomething}>Do Something</button>
+                <br />
+            </div>
         );
     }
+    
 }
 
 export default EventManager
