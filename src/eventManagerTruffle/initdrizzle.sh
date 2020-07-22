02c7abc70c72d714e8cde40f4c1a2ae67b48b418
ShaSch <shane.gc@web.de>
2020-07-22 22:37:35 +0200

    eventManagerTruffle: removed initdrizzle (test)

--- src/eventManagerTruffle/initdrizzle.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-mkdir dapp && cd dapp 
-npm init -y
-npm install react react-dom react-router-dom react-scripts drizzle
-mkdir public 
-mkdir src && cd src 
-ln -s ../../build/contracts contracts
