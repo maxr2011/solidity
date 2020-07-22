0ac2747d4f8175a1443344358edbed67fa5df0c2
ShaSch <shane.gc@web.de>
2020-07-22 13:04:49 +0200

    eventManagerTruffle: added example test case

---
+++ src/eventManagerTruffle/test/test01.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.25 <0.7.0;
+
+// Test Example (MetaCoin)
+
+/*
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/MetaCoin.sol";
+
+contract TestMetaCoin {
+
+  function testInitialBalanceUsingDeployedContract() public {
+    MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
+
+    uint expected = 10000;
+
+    Assert.equal(meta.getBalance(msg.sender), expected, "Owner should have 10000 MetaCoin initially");
+  }
+
+  function testInitialBalanceWithNewMetaCoin() public {
+    MetaCoin meta = new MetaCoin();
+
+    uint expected = 10000;
+
+    Assert.equal(meta.getBalance(msg.sender), expected, "Owner should have 10000 MetaCoin initially");
+  }
+
+}
+*/
