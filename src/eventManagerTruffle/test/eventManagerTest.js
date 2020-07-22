9ff861dbc081e36302d57958f78e58c615fd76b2
Valentin <valentin.zitzmann@student.fhws.de>
2020-07-22 23:27:55 +0200

    added event-test and refactored

--- src/eventManagerTruffle/test/eventManagerTest.js
+++ src/eventManagerTruffle/test/eventManagerTest.js
@@ -1,20 +1,19 @@
-const { assert } = require("console");
-
 const EventManager = artifacts.require('EventManager');
 const Event = artifacts.require('Event');
 
 contract('eventManager', accounts => {
-    let eventManager = null;
-    let admin = accounts[0];
-    let userEvents = [0,0,0,0,0];
     before(async () => {
         eventManager = await EventManager.deployed();
+        myAccounts = accounts;
+        admin = accounts[0];
         // register half the accounts.
         await eventManager.register({from: admin}); 
         await eventManager.register({from: accounts[1]}); 
         await eventManager.register({from: accounts[2]}); 
         await eventManager.register({from: accounts[3]}); 
         await eventManager.register({from: accounts[4]}); 
+
+        userEvents = [0,0,0,0,0];
     });
     it('should be deployable', async () => {
         assert(eventManager.address !== '');
@@ -34,8 +33,7 @@ contract('eventManager', accounts => {
         }
         catch (err) {
             assert(err.message.includes('revert'),'Error message did not include revert');
-        }
-        
+        }        
     });
     it('should allow new Events', async () => {
         await createDummyEvent(admin);
@@ -67,34 +65,65 @@ contract('eventManager', accounts => {
             assert(events.length === userEvents[i], 'userEventCount does not match the number of created events.');
         }
     });
-    async function getEventCount (account = admin){
-        return await eventManager.getEventCount({from: account});
-    }
-    function compareEventInfoWithDummy(eventInfo, eventAddress, account)
-    {
-        assert(eventAddress === eventInfo[0], 'Event-Id wasn´t set to the contract address.');
-        assert(account === eventInfo[1], 'Event-Initiator wasn´t stored.');
-        assert(name === eventInfo[2], 'Event-Name wasn´t stored.');
-        assert(location === eventInfo[3], 'Event-Location wasn´t stored.');
-        assert(startTime == eventInfo[4], 'Event-Start-Time wasn´t stored.');
-        assert(endTime == eventInfo[5], 'Event-End-Time wasn´t stored.');
-    }
-    async function getAllEvents(){
-        return await eventManager.getAllEvents({from: admin});
-    }
-    let startTime = 2595424765, endTime = 3595424765;
-    let name = 'Bingo', location = 'Park';
-    async function createDummyEvent(account)
-    {
-        userEvents[accounts.indexOf(account)]++;
-        await eventManager.createUserEvent(name,location,startTime,endTime, {from: account});
-    }
-    async function userCount()
-    {
-        return await eventManager.getUserCount({from: admin});
-    }
-    async function getUserById(id)
-    {
-        return await eventManager.getUserById(id, {from:admin})
-    }
 });
+// Events can only be accessed trough EventManager, but here are the Event-focused tests.
+contract('event', accounts => {
+    let event = null;
+    let initiator = accounts[1];
+    before(async () => {
+        eventManager = await EventManager.deployed();
+
+        for(i = 0; i < accounts.length; i++)
+            await eventManager.register({from: accounts[i]}); 
+
+        await createDummyEvent(initiator);
+        const eventAddress = await eventManager.getEventById(await getEventCount(), {from: admin});
+        assert(eventAddress !== '', 'Event Address is null.');
+        event = await Event.at(eventAddress);
+    });
+    it('shouldn´t accept initiator as participant.', async () => {
+        
+        try {
+            await eventManager.participateEventById(event, {from: initiator});
+            assert.fail('Initiator was able to participate at his own event.');
+        }
+        catch (err) {
+            assert(err.message.includes('you are already the initiator'),'Error message did not include revert-messages: ');
+        }     
+    })
+});
+
+
+let admin = null, myAccounts = null, eventManager = null;
+async function getEventCount (account = admin){
+    return await eventManager.getEventCount({from: account});
+}
+function compareEventInfoWithDummy(eventInfo, eventAddress, account)
+{
+    assert(eventAddress === eventInfo[0], 'Event-Id wasn´t set to the contract address.');
+    assert(account === eventInfo[1], 'Event-Initiator wasn´t stored.');
+    assert(name === eventInfo[2], 'Event-Name wasn´t stored.');
+    assert(location === eventInfo[3], 'Event-Location wasn´t stored.');
+    assert(startTime == eventInfo[4], 'Event-Start-Time wasn´t stored.');
+    assert(endTime == eventInfo[5], 'Event-End-Time wasn´t stored.');
+}
+async function getAllEvents(){
+    return await eventManager.getAllEvents({from: admin});
+}
+let startTime = 2595424765, endTime = 3595424765;
+let name = 'Bingo', location = 'Park';
+let userEvents = [0,0,0,0,0];
+let _accounts = null;
+async function createDummyEvent(account)
+{
+    userEvents[myAccounts.indexOf(account)]++;
+    await eventManager.createUserEvent(name,location,startTime,endTime, {from: account});
+}
+async function userCount()
+{
+    return await eventManager.getUserCount({from: admin});
+}
+async function getUserById(id)
+{
+    return await eventManager.getUserById(id, {from:admin})
+}
