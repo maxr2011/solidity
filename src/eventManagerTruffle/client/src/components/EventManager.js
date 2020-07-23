cf880fd593763d31f6fe9e7c60b22ae41ad76d55
Maximilian Rehberger <maxi.rehberger@gmail.com>
2020-07-23 12:31:23 +0200

    eventManagerTruffle: App updated. LoadingScreen now disappears. EventManager Module loaded.

--- src/eventManagerTruffle/client/src/components/EventManager.js
+++ src/eventManagerTruffle/client/src/components/EventManager.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-function doSomething() {
-    console.log("do something log");
-}
+import './EventManager.css';
 
 class EventManager extends React.Component {
 
@@ -10,16 +8,34 @@ class EventManager extends React.Component {
         console.log("do something");
     }
 
+    login() {
+        console.log("logging in..");
+    }
+    
+    register() {
+        console.log("register new user...");
+    }
+
     render() {
         return (
-            <div id="buttonWrapper">
-                <h2>EventManager Test</h2>
-                <button onClick={this.doSomething}>Do Something</button>
+            <div id="eventManager">
+            <div id="eventManagerSection">
+                <h2>EventManager</h2>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td valign="top"><button id="login_button" onClick={this.login}>Login</button></td>
+                        <td valign="top"><button id="register_button" onClick={this.register}>Register</button></td>
+                    </tr>
+                    </tbody>
+                </table>
+                
                 <br />
+            </div>
             </div>
         );
     }
-    
+
 }
 
 export default EventManager
