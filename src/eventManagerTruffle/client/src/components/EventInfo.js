32a9d7b2a6059d77cc828be09281fa005c7a226b
Maximilian Rehberger <maxi.rehberger@gmail.com>
2020-07-24 02:40:21 +0200

    eventManagerTruffle: show event participants, date convert, remove event

--- src/eventManagerTruffle/client/src/components/EventInfo.js
+++ src/eventManagerTruffle/client/src/components/EventInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 class EventInfo extends React.Component {
-    state = { eventInfo: null, drizzleState: null, drizzle: null };
+    state = { eventInfo: null, eventParticipants: null, drizzleState: null, drizzle: null };
 
     componentDidMount() {
 
@@ -14,6 +14,29 @@ class EventInfo extends React.Component {
             this.setState({ loading: false, drizzleState , drizzle });
 
         }
+
+    }
+
+    timestampConvert(unix_timestamp) {
+
+        var a = new Date(unix_timestamp * 1000);
+        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var hour = a.getHours();
+        var min = a.getMinutes();
+        var sec = a.getSeconds();
+
+        if(min < 10) {
+            min = '0'+min;
+        }
+        if(sec < 10) {
+            sec = '0'+sec;
+        }
+
+        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+        return time;
 
     }
 
@@ -36,7 +59,45 @@ class EventInfo extends React.Component {
                 console.log("event info");
                 console.log(eventInfo);
 
-                document.getElementById('event_info').innerHTML= "Initiator: " + eventInfo['event_initiator'] + "<br /> Name: " + eventInfo['event_name'] + "<br /> Ort: " + eventInfo['event_location'] + "<br /> Startzeit: " + eventInfo['event_start_time'] + "<br /> Endzeit: " + eventInfo['event_end_time'] + "<br />";
+                document.getElementById('event_info').innerHTML= "Initiator: " + eventInfo['event_initiator'] + "<br /> Name: " + eventInfo['event_name'] + "<br /> Ort: " + eventInfo['event_location'] + "<br /> Startzeit: " + this.timestampConvert(eventInfo['event_start_time']) + "<br /> Endzeit: " + this.timestampConvert(eventInfo['event_end_time']) + "<br />";
+                
+            });
+
+        }
+
+    }
+
+    eventParticipants = () => {
+
+        console.log('showing event participants ...'); // getEventParticipants
+
+        var drizzle = this.state.drizzle;
+
+        var event_info_address = document.getElementById('event_info_address').value;
+
+        const contract = drizzle.contracts.EventManager;
+
+        var state = drizzle.store.getState();
+
+        if(state.drizzleStatus.initialized) {
+
+            contract.methods.getEventParticipants(event_info_address).call().then(eventParticipants => {
+
+                this.setState({ eventParticipants });
+                console.log("event participants");
+                console.log(eventParticipants);
+
+                var event_participants_string = "";
+
+                for(var i = 0; i < eventParticipants.length; i++) {
+                    event_participants_string += eventParticipants[i] + " <br />";
+                }
+
+                if(eventParticipants.length < 1) {
+                    document.getElementById('participants_info').innerHTML = "<br />Keine Teilnehmer <br />";
+                } else {
+                    document.getElementById('participants_info').innerHTML = "<br />Teilnehmer: " + event_participants_string + "<br />";
+                }                
                 
             });
 
@@ -63,12 +124,32 @@ class EventInfo extends React.Component {
 
     }
 
+    eventRemove = () => {
+
+        console.log('deleting event ...');
+
+        var drizzle = this.state.drizzle;
+
+        var event_remove_address = document.getElementById('event_info_address').value;
+
+        var state = drizzle.store.getState();
+
+        if(state.drizzleStatus.initialized) {
+
+            const contract = this.state.drizzle.contracts.EventManager;
+            contract.methods.removeEvent(event_remove_address).send();
+
+        }
+
+    }
+
     render() {
 
         return(
             <div id="event_info_div">
                 
                 <div id="event_info"></div>
+                <div id="participants_info"></div>
                 <form>
                     <label for="event_info_address" id="input_label">Adresse:</label> <input type="text" name="event_info_address" id="event_info_address"></input> <br />
                 </form>
@@ -76,7 +157,9 @@ class EventInfo extends React.Component {
                     <tbody>
                     <tr>
                         <td valign="top"><button id="event_info_button" onClick={this.eventInfo}>Info anzeigen</button></td>
+                        <td valign="top"><button id="event_info_participants_button" onClick={this.eventParticipants}>Teilnehmer anzeigen</button></td>
                         <td valign="top"><button id="event_participate_button" onClick={this.eventParticipate}>Teilnehmen</button></td>
+                        <td valign="top"><button id="event_remove_button" onClick={this.eventRemove}>Event Löschen</button></td>
                     </tr>
                     </tbody>
                 </table>
